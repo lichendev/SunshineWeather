f6a8377fd681f4d9e956cdcaab397f537666a739
lichendev <lichenlzc@outlook.com>
2021-07-07 14:19:08 +0800

    android-init

---
+++ app/src/main/java/com/example/sunshineweather/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.sunshineweather
+
+import android.content.ContentValues
+import android.content.Context
+import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.example.sunshineweather.databinding.ActivityMainBinding
+import java.lang.Exception
+import java.util.*
+import kotlin.collections.ArrayList
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
